2296b89c0a34b3070aaeb4c9b49721df0afa6ca6
Subhajit Mandal <108950928+Subhajit-mn@users.noreply.github.com>
2023-11-03 10:01:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
 function threeSum(arr, target) {
 // write your code here
-	let initialDiff = Number.MAX_VALUE, ans = -1;
+	let closestSum = Number.MAX_VALUE;
 	let n = arr.length;
 	for(let i=0; i<n-2; i++){
 		for(let j=i+1; j<n-1; j++){
 			for(let k=j+1; k<n; k++){
 				let sum = arr[i]+arr[j]+arr[k];
-				let diff = Math.abs(sum - target);
-				if(diff < initialDiff){
-					ans = sum;
+				if(Math.abs(target-closestSum) > Math.abs(target-sum)){
+					closestSum = sum;
 				}
 			}
 		}
 	}
-	return sum;
+	return closestSum;
 }
 
 module.exports = threeSum;
